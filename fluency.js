88577734fd063227611b9dd30dc32c4ffc6db88e
Theo <theo@interactivedocs.com>
2014-10-17 21:41:10 -0700

    Added a JS file
    Branded loader.

---
+++ fluency.js
@@ -0,0 +1,11 @@
+/*
+ * Fluency v0.1.0 (http://interactivedocs.com/fluency)
+ * Copyright 2014 Interactive Docs
+ * Licensed under MIT (https://github.com/interactivedocs/fluency/blob/master/LICENSE)
+ */
+
+/* Branded loader */
+$(window).load(function() {
+    $('.loader-overlay').fadeOut();
+    $('.loader-content').fadeOut();
+});
