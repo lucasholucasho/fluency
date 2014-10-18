e4655978151df6f23dcdc1a6d2be0187748a2652
Theo <theo@interactivedocs.com>
2014-10-18 15:55:52 -0700

    docs

---
+++ docs/framework/fluency.js
@@ -0,0 +1,18 @@
+/*
+ * Fluency v0.1.0 (http://interactivedocs.com/fluency)
+ * Copyright 2014 Interactive Docs
+ * Licensed under MIT (https://github.com/interactivedocs/fluency/blob/master/LICENSE)
+ */
+
+/* Branded loader */
+$(window).load(function() {
+    $('.loader-overlay').fadeOut();
+    $('.loader-content').fadeOut();
+});
+
+
+
+
+<ul class="playlist-gallery horizontal-small">
+    <li data-timecode="5600" data-duration=""></li>
+</ul>
